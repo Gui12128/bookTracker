92d7c28305a5bdbfcb5e8b1564f2f1b88becfa66
Guilherme Fonseca <79527437+Gui12128@users.noreply.github.com>
2024-07-06 20:45:17 -0300

    Add files via upload

---
+++ book-tracker/book-tracker/src/test/java/br/com/book_tracker/book_tracker/BookTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.book_tracker.book_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
